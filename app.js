9adff399b47dbdacf2dc41b761fc4dc52ed98471
Edmund D <52433772+Iowa20@users.noreply.github.com>
2020-04-10 18:32:26 -0400

    update

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 function animatedForm(){
- const skulls = document.querySelectorAll('fa-skull');
+ const skulls = document.querySelectorAll(".fa-skull");
  
  skulls.forEach(skulls => {
     skulls.addEventListener("click", () => {
         const input = skulls.previousElementSibling;
         const parent = skulls.parentElement;
+        const nextForm = parent.nextElementSibling;
+
+        //check for validation
+        if(input.type === "text" && validateUser(input)){
+            nextForm(parent, nextForm);    
+        }
     });
  });
 }
 
 
+function validateUser(user) {
+    if (user.value.length < 6){
+        console.log("not enough characters");
+        error("rgb(189,87,87)");
+    }
+        else {
+            error("rgb (50, 100, 83)");
+            return true;
+        }
+    }
+
+function nextSlide(parent, nextForm){
+    parent.classList.add('innactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+}
+
+
+function error(color){
+    document.body.style.backgroundColor = color;
+}
 
 
 
