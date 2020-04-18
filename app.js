0f9df4a643e9a7f1e503b888fc39dde78ac343c8
Edmund D <52433772+Iowa20@users.noreply.github.com>
2020-04-18 11:06:46 -0400

    update

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ function animatedForm(){
         //check for validation
         if(input.type === "text" && validateUser(input)){
             nextSlide(parent, nextForm);    
+        } else if(input.type === "email" && validateEmail(input)){
+            nextSlide(parent, nextForm);
+        } else if(input.type === "password" && validateUser(input)){
+            nextSlide(parent, nextForm);
+        }else{
+            parent.style.animation = "shake 0.5s ease;"
         }
+        //get rid of animation
+        parent.addEventListener('animation', () => {
+            parent.style.animation = " ";
+        })
     });
  });
 }
@@ -26,6 +36,16 @@ function validateUser(user) {
             return true;
         }
     }
+
+function validateEmail(email){
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(validation.test(email.value)){
+        error("rgb(50,100,83)");
+        return true;
+    } else {
+        error("rgb (189, 87, 87)");
+    }
+}
 
 function nextSlide(parent, nextForm){
     parent.classList.add('innactive');
